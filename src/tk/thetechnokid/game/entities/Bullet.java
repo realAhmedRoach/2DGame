e7494fa2d108dcc6e6a45ce437f682a5be6ae2e8
theTechnoKid <theTechnoKid11@gmail.com>
2015-02-26 17:34:23 -0500

    Added bullet timing

--- src/tk/thetechnokid/game/entities/Bullet.java
+++ src/tk/thetechnokid/game/entities/Bullet.java
@@ -16,6 +16,8 @@ public class Bullet extends Entity {
 	public boolean drawn;
 	private int xdir, ydir;
 
+	private long startTime;
+	
 	static {
 		try {
 			image = ImageIO.read(new File("res/bullet.png"));
@@ -28,6 +30,7 @@ public class Bullet extends Entity {
 	public Bullet(Creature parent, Creature target) {
 		super(parent.x, parent.y, image);
 		this.target = target;
+		startTime = System.currentTimeMillis();
 		if(target.x == x && target.y == y) destroyed  = true;
 	}
 
@@ -37,6 +40,10 @@ public class Bullet extends Entity {
 
 	@Override
 	public void tick() {
+		if(System.currentTimeMillis()-startTime>500) {
+			destroyed = true;
+			return;
+		}
 		if (target.y > y) ydir = 1;
 		if (target.y < y) ydir = -1;
 		if (target.y == y) ydir = 0;
@@ -46,7 +53,7 @@ public class Bullet extends Entity {
 		y += SPEED * ydir;
 		x += SPEED * xdir;
 		if (Math.abs(target.x - x) <= 2 && Math.abs(target.y - y) <= 2) {
-			EntityController.entities.remove(target);
+			EntityController.removeEntity(target);
 			destroyed = true;
 		}
 	}
