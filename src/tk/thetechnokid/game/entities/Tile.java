8f88e5427383c3b435288ad283120c3f88477296
theTechnoKid <theTechnoKid11@gmail.com>
2015-03-23 15:00:40 -0400

    Removed 'solid' atrribute of tiles

--- src/tk/thetechnokid/game/entities/Tile.java
+++ src/tk/thetechnokid/game/entities/Tile.java
@@ -21,31 +21,25 @@ public class Tile extends Entity {
 		}
 	}
 	// TILES
-	public static Tile VOID = new Tile(s.crop(0, 0), false);
-	public static Tile GRASS = new Tile(s.crop(1, 0), false);
-	public static Tile ROCK = new Tile(s.crop(2, 0), true);
-	public static Tile WATER1 = new Tile(s.crop(3, 0), false);
-	public static Tile WATER2 = new Tile(s.crop(4, 0), false);
-	public static Tile DIRT = new Tile(s.crop(5, 0), false);
-	public static Tile PORTAL = new Tile(s.crop(6, 0), false);
-	public static Tile TREE = new Tile(s.crop(7, 0), false);
+	public static Tile VOID = new Tile(s.crop(0, 0));
+	public static Tile GRASS = new Tile(s.crop(1, 0));
+	public static Tile ROCK = new Tile(s.crop(2, 0));
+	public static Tile WATER1 = new Tile(s.crop(3, 0));
+	public static Tile WATER2 = new Tile(s.crop(4, 0));
+	public static Tile DIRT = new Tile(s.crop(5, 0));
+	public static Tile PORTAL = new Tile(s.crop(6, 0));
+	public static Tile TREE = new Tile(s.crop(7, 0));
 	// END TILES
 
 	BufferedImage image;
-	boolean solid;
 
-	public Tile(BufferedImage image, boolean solid) {
+	public Tile(BufferedImage image) {
 		super(0, 26, image);
-		this.solid = solid;
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(sprite, x, y, null);
-	}
-
-	public boolean isSolid() {
-		return solid;
 	}
 
 	@Override
